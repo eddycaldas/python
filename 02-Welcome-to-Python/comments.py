17ebf01851c519f154d2fffcb9a0af0e9b93fa94
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-10 09:51:48 -0700

    comments #

---
+++ 02-Welcome-to-Python/comments.py
@@ -0,0 +1,11 @@
+print("Good morning")
+#print("Good afternoon")
+print("Good night")
+
+#this perform a mathematical calculation
+print(1 + 1) #another comment
+
+# print(1 + 1)
+# print(1 + 1)
+# print(1 + 1)
+
