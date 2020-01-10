097a76ad2c572dbcddffa3462014a6b81ee7c2e2
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-10 10:00:20 -0700

    practice: sep=

--- 02-Welcome-to-Python/comments.py
+++ 02-Welcome-to-Python/comments.py
@@ -9,3 +9,6 @@ print(1 + 1) #another comment
 # print(1 + 1)
 # print(1 + 1)
 
+print("Summer", "Fall", "Winter", "Spring", sep= "**")
+print(1, 2, 1, 2, sep='3')
+print(1 + 2, 3 + 4, 4 + 5, sep="=")
