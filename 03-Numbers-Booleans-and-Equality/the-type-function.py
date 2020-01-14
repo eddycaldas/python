9a25f2db65e1b785ac9d376f446c7df9f3fc8905
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-14 11:49:25 -0700

    type function

---
+++ 03-Numbers-Booleans-and-Equality/the-type-function.py
@@ -0,0 +1,14 @@
+# an object is an instance of a class.
+
+# a class represent a blueprint from which objects are made.
+
+print(type(5))
+print(type('food'))
+print(type(True))
+print(type(5.3))
+
+print(type(4) == type(10))
+print(type(5) == type(5.0))
+
+print(type([1, 2, 3]))
+print(type({ 'NJ': 'No Jode'}))
