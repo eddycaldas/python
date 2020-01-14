f4a315f19652a445a20a9ebc0264796279c48d52
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-14 12:43:33 -0700

    intro to variables

---
+++ 04-Variables/intro-to-variables.py
@@ -0,0 +1,15 @@
+name = "Tacos"
+age = 27
+handsome = "True"
+favorite_language = "Python"
+
+print(name)
+print(age)
+print(handsome)
+print(favorite_language)
+
+print("My name is", name, "and I am", age, "years old")
+
+age = 23
+print(age)
+
