443551084642f4bfe92a0ee915b8313f8e7ec371
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-15 10:20:22 -0700

    parameters and arguments

---
+++ 05-Functions/parameters-and-arguments.py
@@ -0,0 +1,14 @@
+# parameter is the name of an expected input
+# argument is actual value provided
+
+def p(text):
+    print(text)
+
+p("Hello")
+p("Goodbye")
+
+
+def add(a, b):
+    print('the sum of', a, 'and', b, 'is', a + b)
+
+add(4, 5)
