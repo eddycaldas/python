7fd1b67c0fbb63f18ac68a569dc607d043b825a4
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-15 09:42:20 -0700

    error exceptions

---
+++ 04-Variables/assignment-convert-fahrenheit-temperature-to-celsius.py
@@ -0,0 +1,4 @@
+temp_f = float (input("What's the temperature in Fahrenheit? ") )
+
+temp_c = (temp_f - 32) * 5/9
+print('Temp in Celsious is ', temp_c)
---
+++ 04-Variables/the-NameError-ValueError-TypeError-and-SyntaxError-Exceptions.py
@@ -0,0 +1,12 @@
+### name error exception:
+# first_name = "Eddy"
+# print(firstname)
+
+### type error
+# print("5" + 3)
+
+### Value error
+# int("xyz")
+
+### syntax error
+# print('hello)
