7951fee751162bbf2b545cff28872ef54e793b31
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-14 17:12:28 -0700

    user input function

---
+++ 04-Variables/user-input-with-the-input-function.py
@@ -0,0 +1,14 @@
+# first_name = input("What's your name? ")
+# print("Nice to meet you", first_name)
+
+#prompt the user for two numbers, one at a time
+num1 = input("provide a first number ")
+num2 = input("provide a second number ")
+
+# The number will be received as a string
+# convert those two numbers to integers
+num11 = int(num1)
+num22 = int(num2)
+
+# print a message that includes the sum of the two numbers
+print("The addition of", num1, "and", num2, "is", num11 + num22)
