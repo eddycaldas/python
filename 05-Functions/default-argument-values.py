e9702e4f71e18cf66207ac58194252b64093d6b2
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-17 10:56:13 -0700

    default arguments

---
+++ 05-Functions/default-argument-values.py
@@ -0,0 +1,6 @@
+def add(a = 0, b = 0):
+    return a + b
+
+print(add(3 , 7))
+print(add(7))
+print(add())
--- 05-Functions/return-values.py
+++ 05-Functions/return-values.py
@@ -1,5 +1,12 @@
-def add(a, b):
-    return a + b
+# Define a convert_to_currency function that accepts 
+# a single parameter (an integer). 
+# The function should convert the argument to a string, 
+# prefix it with a dollar sign, and return the result.
+# Example: convert_to_currency(15) => "$15"
 
-result = add(3, 9)
-print(result)
+def convert_to_currency(x):
+    m = int(x)
+    n = "$" + str(m)
+    print('"' + n +  '"')
+
+convert_to_currency(15)
