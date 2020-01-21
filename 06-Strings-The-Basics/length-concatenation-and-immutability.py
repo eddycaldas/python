6dd444974005381255b80b87e9dd8e8682a17792
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-21 11:06:59 -0700

    exercices

--- 06-Strings-The-Basics/length-concatenation-and-immutability.py
+++ 06-Strings-The-Basics/length-concatenation-and-immutability.py
@@ -12,3 +12,44 @@ print('-|' * 8)
 
 # numbers, float, boolans and strings are  immurable - incapable
 # of changing.
+
+## -----------------------
+
+# Define a long_word function that accepts a string. 
+# The function should return a Boolean that reflects 
+# whether the string has more than 7 characters.
+# 
+# EXAMPLES:
+# long_word("Python") => False
+# long_word("magnificent") => True
+
+def long_word(str):
+    if len(str) > 7:
+        print(True)
+    elif len(str) < 8:
+        print(False)
+
+long_word("magnificent")
+long_word("Python")
+
+
+# Define a first_longer_than_second function that accepts 
+# two string arguments. 
+# The function should return a True if the first string is 
+# longer than the second 
+# and False otherwise (including if they are equal in length).
+#
+# EXAMPLES:
+# first_longer_than_second("Python", "Ruby")   => True
+# first_longer_than_second("cat", "mouse")     => False
+# first_longer_than_second("Steven", "Seagal") => False
+
+def first_longer_than_second(str1, str2):
+    if len(str1) > len(str2):
+        print(True)
+    elif len(str1) <= len(str2):
+        print(False)
+
+first_longer_than_second("Python", "Ruby")
+first_longer_than_second("cat", "mouse")
+first_longer_than_second("Steven", "Seagal")
