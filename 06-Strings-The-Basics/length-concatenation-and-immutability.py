9d48f014e63cf80c77f6e4587cc2c825b264a81a
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-21 10:33:11 -0700

    length, concatenation and immutability

---
+++ 06-Strings-The-Basics/length-concatenation-and-immutability.py
@@ -0,0 +1,14 @@
+print(len('tacos'))
+print(len('njk 434'))
+print(len(' '))
+print(len(''))
+
+print('Quiero' + 'Tacos')
+print('Quiero ' + 'Tacos')
+print('Quiero' + ' Tacos')
+print('Quiero' + ' ' + 'Tacos')
+print('-|' * 8)
+
+
+# numbers, float, boolans and strings are  immurable - incapable
+# of changing.
