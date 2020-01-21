7bc2ad9152515104ca3b5b021b53953f94d468f5
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-21 09:39:40 -0700

    none type

--- 05-Functions/default-argument-values.py
+++ 05-Functions/default-argument-values.py
@@ -1,6 +1,13 @@
-def add(a = 0, b = 0):
-    return a + b
+# def string_adder(a = '', b = ''):
+#     return str(a) + " " + str(b)
 
-print(add(3 , 7))
-print(add(7))
-print(add())
+# print(string_adder(3, 5))
+# print(string_adder())
+
+
+
+def multiplier(a = 1, b = 1, c = 1):
+    return a * b * c
+    
+print(multiplier(2,3,4))
+print(multiplier())
---
+++ 05-Functions/the-none-type.py
@@ -0,0 +1,11 @@
+a = None
+print(type(a))
+
+def substract(a, b):
+    print(a - b)
+
+result = substract(7, 3)
+print(result)
+
+# due to not have a return statement, by printing 
+# result, it prints out None.
