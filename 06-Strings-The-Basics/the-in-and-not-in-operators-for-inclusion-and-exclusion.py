b0a606251830d67b9e959877a3faa476f5894a49
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-29 09:34:06 -0700

    in and not it operators

---
+++ 06-Strings-The-Basics/the-in-and-not-in-operators-for-inclusion-and-exclusion.py
@@ -0,0 +1,11 @@
+announcement = "The winners of the competation are Taco, Steve and Anna"
+
+print("Taco" in announcement)
+print("taco" in announcement)
+print("Steve" in announcement)
+
+print()
+
+print("Taco" not in announcement)
+print("taco" not in announcement)
+print("Steve" not in announcement)
