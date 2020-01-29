6ef9798512b3d4a98fad9867c72d720f47b1dc8d
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-29 09:13:26 -0700

    special characters

---
+++ 06-Strings-The-Basics/escape-characters.py
@@ -0,0 +1,24 @@
+# create new line
+print("this will \ncreate a \nnew line")
+
+# tab 
+print("\tlook at that \tpicture")
+
+# quotes inside a quotes
+print("\"To be or not to be\", said someone")
+
+# backslash
+print("let's print a backslash: \\")
+
+# raw: r
+file_name = r"c:\news\travel"
+print(file_name)
+
+# backslash to break a long line
+some_random_number = 4
+another_random_number = 9
+probalby_another_random_number = 23
+
+my_total = some_random_number + \
+           another_random_number + \
+           probalby_another_random_number
