01ec2e11919a1a060ce6c7e586abc0461705c56c
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-01-31 12:03:22 -0700

    count method

---
+++ 07-Strings-Methods/the-count-method.py
@@ -0,0 +1,10 @@
+
+my_string = 'queueing'
+
+print(my_string.count('u'))
+print(my_string.count('ue'))
+print(my_string.count('q'))
+print()
+print(my_string.count('r'))
+print()
+print(my_string.count('u') + my_string.count('e'))
