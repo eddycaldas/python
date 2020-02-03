7c7664a2af4a1e610f628f2cc9f83226ca984dc0
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-03 09:52:20 -0700

    capitalize, title, ...methods

---
+++ 07-Strings-Methods/the-capitalize-title-lower-upper-and-swapcase-methods.py
@@ -0,0 +1,29 @@
+story = "once upon a time"
+
+# this method will capitalize the first letter
+# of the whole string
+print(story.capitalize())
+
+#this method will capitalize every first letter
+#of every word on the string
+print(story.title())
+
+#capitalize every letter on string
+print(story.upper())
+
+print()
+
+print("HELLO".lower())
+
+print()
+
+#swapcase method
+print("AbCdE".swapcase())
+
+#Using multiple methods
+print("COLORADO SPRINGS".lower().title())
+
+print()
+
+story = story.title()
+print(story)
--- 07-Strings-Methods/the-count-method.py
+++ 07-Strings-Methods/the-count-method.py
@@ -1,10 +1,43 @@
+# Define a vowel_count function that accepts a string argument.
+# The function should return the count of vowels in the string.
+# The 5 vowels are "a", "e", "i", "o", and "u".
+# You can assume the string will be in all lowercase.
+#
+# EXAMPLES:
+# vowel_count("estate")     => 3
+# vowel_count("helicopter") => 4
+# vowel_count("ssh")        =>
 
-my_string = 'queueing'
+def vowel_count(str):
+    print(str.count('a') +
+          str.count('e') +
+          str.count('i') +
+          str.count('o') +
+          str.count('u')  )
 
-print(my_string.count('u'))
-print(my_string.count('ue'))
-print(my_string.count('q'))
+vowel_count("estate")
+vowel_count("helicopter")
+vowel_count("ssh")
 print()
-print(my_string.count('r'))
 print()
-print(my_string.count('u') + my_string.count('e'))
+print()
+#  -------------------------------------->
+
+# Define a find_my_letter function that accepts 
+# two arguments: a string and a character
+# The function should return the first index position 
+# of the character in the string
+# The function should return a -1 if the character 
+# does not exist in the string
+#
+# EXAMPLES:
+# find_my_letter("dangerous", "a") => 1
+# find_my_letter("bazooka", "z") => 2
+# find_my_letter("lollipop", "z") => -1
+
+def find_my_letter(str, x):
+    print(str.find(x))
+
+find_my_letter("dangerous", "a")
+find_my_letter("bazooka", "z")
+find_my_letter("lollipop", "z")
