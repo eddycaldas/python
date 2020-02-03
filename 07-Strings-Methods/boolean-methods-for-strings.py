354037b835db0410b393219214d3aadb037d5ed3
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-03 10:09:17 -0700

    boolean methods for strings

---
+++ 07-Strings-Methods/boolean-methods-for-strings.py
@@ -0,0 +1,33 @@
+print("winter".islower())
+print("winter 233$".islower())
+print("Winter".islower())
+
+print()
+
+print("WELCOME".isupper())
+print("wELCOME".isupper())
+
+print()
+
+print('The Four Seasons'.istitle())
+
+print()
+
+print('area'.isalpha())
+print('Area'.isalpha())
+print('Area 51'.isalpha())
+
+print()
+
+print('2345245'.isnumeric())
+
+print()
+
+print('area51'.isalnum())
+print('area 51'.isalnum())
+print()
+
+print(' '.isspace())
+print('   '.isspace())
+print(' 4 '.isspace())
+print(' ! '.isspace())
