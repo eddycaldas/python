7886a2b4f8039f50624258e705f036142d000b6e
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-06 12:31:22 -0700

    formatted string literals

---
+++ 07-Strings-Methods/formatted-string-literals.py
@@ -0,0 +1,11 @@
+name = "bobby"
+adjective = "smelly"
+noun = "fish"
+
+mad_libs = f"{name} laughed at the {adjective} {noun}"
+print(mad_libs)
+
+mad_libs = F"{name} laughed at the {adjective} {noun}"
+print(mad_libs)
+
+print(f"2 + 2 is { 2 + 2}")
