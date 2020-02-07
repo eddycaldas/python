0237c6a26c83212684b27ee4629b5a113ccb02e5
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-07 09:26:25 -0700

    bool method with True or False

---
+++ 08-Control-Flow/the-if-statement.py
@@ -0,0 +1,14 @@
+# zero or empty string are falsie values, all other numbers are not
+
+if 3:
+    print('Yes!')
+
+if 0:
+    print("humm...")
+
+print()
+
+print(bool(0))
+print(bool(-1))
+print(bool(""))
+print(bool(" "))
