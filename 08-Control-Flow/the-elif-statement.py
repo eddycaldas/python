28991abf455c3ab7fac443fefb7bb84461e2cb45
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-07 15:20:33 -0700

    if, elif statement

---
+++ 08-Control-Flow/the-elif-statement.py
@@ -0,0 +1,11 @@
+def positive_or_negative(num):
+    if num > 0:
+        print("Positive!")
+    elif num < 0:
+        print("Negative!")
+    else:
+        print("neither, it is a zero")
+
+positive_or_negative(4)
+positive_or_negative(-3)
+positive_or_negative(0)
--- 08-Control-Flow/the-if-statement.py
+++ 08-Control-Flow/the-if-statement.py
@@ -1,14 +1,57 @@
-# zero or empty string are falsie values, all other numbers are not
+# # zero or empty string are falsie values, all other numbers are not
 
-if 3:
-    print('Yes!')
+# if 3:
+#     print('Yes!')
 
-if 0:
-    print("humm...")
+# if 0:
+#     print("humm...")
+
+# print()
+
+# print(bool(0))
+# print(bool(-1))
+# print(bool(""))
+# print(bool(" "))
+
+# Define a even_or_odd function that accepts a single integer.
+# If the integer is even, the function should return the string “even”.
+# If the integer is odd, the function should return the string “odd”.
+#
+# even_or_odd(2) => "even"
+# even_or_odd(0) => "even"
+# even_or_odd(13) => "odd"
+# even_or_odd(9) => "odd"
+
+def even_or_odd(int):
+    if (int%2 == 0):
+        print("even")
+    elif (int%2 != 0):
+        print('odd')
+
+even_or_odd(2) 
+even_or_odd(0) 
+even_or_odd(13) 
+even_or_odd(9) 
 
 print()
 
-print(bool(0))
-print(bool(-1))
-print(bool(""))
-print(bool(" "))
+# Define a truthy_or_falsy function that accepts a single argument.
+# The function should return a string that reads "The value _____ is ______" 
+# where the first space is the argument and the second space 
+# is either truthy or falsy.
+# 
+# truthy_or_falsy(0) => "The value 0 is falsy"
+# truthy_or_falsy(5) => "The value 5 is truthy"
+# truthy_or_falsy("Hello") => "The value Hello is truthy"
+# truthy_or_falsy("") => "The value  is falsy"
+
+def truthy_or_falsy(arg):
+    if arg == 0 or arg == '':
+        print("The value ", arg, ' is falsy' )
+    else:
+        print('The value ', arg, ' is truthy')
+
+truthy_or_falsy(0)
+truthy_or_falsy(5)
+truthy_or_falsy("Hello")
+truthy_or_falsy("")
