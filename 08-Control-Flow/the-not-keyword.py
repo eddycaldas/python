fc690c4d8548d52d4abc0cbc1bb665f608e7e27f
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-09 18:55:26 -0700

    not keyboard

---
+++ 08-Control-Flow/the-not-keyword.py
@@ -0,0 +1,18 @@
+print(not True)
+print(not False)
+
+if "H" in 'Hello':
+    print("H exist on string")
+
+if 'z' not in 'tacos':
+    print('z does not exist on tacos')
+
+print()
+
+value = 10
+
+if value > 100:
+    print("this will not print")
+
+if not value > 100:
+    print('this will print!!!')
