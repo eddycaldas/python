e7522a6f628cf51c3ede720ae7c88257f759ef85
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-25 11:03:46 -0700

    in and not in operator

---
+++ 09-Lists-The-Basics/the-in-and-not-in-operators-on-a-list.py
@@ -0,0 +1,15 @@
+print('fast' in 'breakfast')
+print ('fast' in 'lunch')
+
+meals = ['breakfast', 'lunch', 'dinner']
+
+print('lunch' in meals)
+print('Dinner' in meals)
+
+print()
+
+print('lunch' not in meals)
+print('snack' not in meals)
+
+if 'snack' not in meals:
+    print("It is not there!!!")
