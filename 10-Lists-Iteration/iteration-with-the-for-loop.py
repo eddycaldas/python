91f985b8786335ea5b37f5187f86bf2b7af346f0
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-02-26 09:56:43 -0700

    intro to for loop

---
+++ 10-Lists-Iteration/iteration-with-the-for-loop.py
@@ -0,0 +1,24 @@
+dinner = 'steak with potatoes'
+
+# for i in dinner:
+#     print(i)
+
+comidas = ['ceviche', 'saltado', 'papitas fritas']
+
+for i in comidas:
+    print(i)
+    print(len(i))
+print('------')
+
+numbers = [1, 4, 7, 23, 45]
+
+for i in numbers:
+    print(i * 2)
+
+print('----------')
+
+total = 0
+for i in numbers:
+    total = total + i
+
+print(total)
