2f9dd8f445eb056cd62fb721f9e8a234f9e7548d
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-03 07:47:37 -0700

    iteration with for loop

--- 10-Lists-Iteration/iteration-with-the-for-loop.py
+++ 10-Lists-Iteration/iteration-with-the-for-loop.py
@@ -22,3 +22,44 @@ for i in numbers:
     total = total + i
 
 print(total)
+
+print("==================")
+
+# Define a sum_of_lengths function that accepts a list of strings.
+
+# The function should return the sum of the string lengths.
+
+# sum_of_lengths(["Hello", "Bob"]) => 8
+# sum_of_lengths(["Nonsense"])     => 8
+# sum_of_lengths(["Nonsense", "or", "confidence"]) => 20
+
+def sum_of_lengths(str):
+    total_length = 0
+    for i in str:
+        total_length = total_length + len(i)
+    return total_length
+
+print(sum_of_lengths(["Hello", "Bob"]))
+print(sum_of_lengths(["Nonsense"]))
+print(sum_of_lengths(["Nonsense", "or", "confidence"]))
+
+print('----------')
+
+# Define a product function that accepts a list of numbers.
+# The function should return the product of the numbers.
+# The list will always have at least one value
+#
+# product([1, 2, 3]) => 6
+# product([4, 5, 6, 7]) => 840
+# product([10]) => 10
+
+def product(my_values):
+    initial_total = 1
+    for i in my_values:
+        initial_total = initial_total * i
+    return initial_total
+
+
+print(product([1, 2, 3]))
+print(product([4, 5, 6, 7]))
+print(product([10]))
