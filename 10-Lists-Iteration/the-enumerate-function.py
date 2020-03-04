f63a0fef0dfc009c090baefeae28b035871f6228
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-04 08:21:42 -0700

    enumerate method

---
+++ 10-Lists-Iteration/the-enumerate-function.py
@@ -0,0 +1,11 @@
+errands = ["Walk Max", "Get breakfast", "Study for a bit", "Go to work"]
+
+print(enumerate(errands))
+
+for index, task in enumerate(errands, 1):
+    print(f"{task} in number {index} on my to do list for today!")
+
+print("------------")
+
+for index, task in enumerate(errands, 100):
+    print(f"{task} in number {index} on my to do list for today!")
