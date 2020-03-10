de0b074edd4e270a491b89abc99204d9dffe90cc
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-09 20:37:29 -0600

    range function

---
+++ 10-Lists-Iteration/the-range-function.py
@@ -0,0 +1,25 @@
+
+for number in range(5):
+    print(number)
+# 0 1 2 3 4 
+
+print('----------')
+
+for number in range(3, 9):
+    print(number)
+# 3 4 5 6 7 8
+
+print('----------')
+
+for number in range(10, 61, 10):
+    print(number)
+#10 20 30 40 50 60
+
+print('----------')
+
+for number in range(11, -5, -2):
+    print(number)
+# 11 9 7 5 3 1 -1 -3
+
+print('----------')
+
