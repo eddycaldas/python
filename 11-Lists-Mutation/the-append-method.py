c25363b03893f181cd366ad99f14f9aa088d6943
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-12 10:38:26 -0600

    append method

---
+++ 11-Lists-Mutation/the-append-method.py
@@ -0,0 +1,12 @@
+# append adds to the end of the list
+
+countries = ['Peru', 'United States', 'Bolivia']
+print(countries)
+print()
+
+countries.append('France')
+print(countries)
+print()
+
+countries.append("Japan")
+print(countries)
