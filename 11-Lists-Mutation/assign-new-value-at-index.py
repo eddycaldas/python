203d3bd0b54df1ee1f30987cf0df5108c3e1c445
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-12 10:10:52 -0600

    assigns new value at index

---
+++ 11-Lists-Mutation/assign-new-value-at-index.py
@@ -0,0 +1,14 @@
+crayons = ['Macaroni and cheese', 'Maximum yellow blue', 'jazzberry blue']
+print(crayons)
+print('----------')
+
+crayons[1] = 'macarena blue'
+print(crayons)
+print('----------')
+
+crayons[0] = 'Tortilla yellow'
+print(crayons)
+print('----------')
+
+crayons[-1] = 'Pinky pie'
+print(crayons)
