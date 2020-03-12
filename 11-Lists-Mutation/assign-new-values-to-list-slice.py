77c320497ed88b300423d932ebe55e91a8211392
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-12 10:20:45 -0600

    assign new values to list with slice

---
+++ 11-Lists-Mutation/assign-new-values-to-list-slice.py
@@ -0,0 +1,9 @@
+cowokers = ['Ryan', 'Bob', 'Jim', 'Pamela', 'Stacy']
+
+cowokers[2:4] = ['Mary', 'Anna']
+print(cowokers)
+
+print('----------')
+
+cowokers[-3:-1] = ['James', 'Rachel']
+print(cowokers)
