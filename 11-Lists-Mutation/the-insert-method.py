335d267b349acd835a1b71b12e3629a4b8e8a0aa
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-16 21:20:15 -0600

    insert method

---
+++ 11-Lists-Mutation/the-insert-method.py
@@ -0,0 +1,8 @@
+# insert method allows to insert a list int hemiddle or the beggining of the list. it takes two arguments; index and object
+
+cars = ['toyota', 'honda', 'bmw']
+
+cars.insert(1, 'subaru')
+
+print(cars)
+
