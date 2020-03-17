852bc5b234390d5450c2fc18568b4f7bd02b87b8
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-17 09:43:52 -0600

    insert method and exxercises

--- 11-Lists-Mutation/the-insert-method.py
+++ 11-Lists-Mutation/the-insert-method.py
@@ -6,3 +6,26 @@ cars.insert(1, 'subaru')
 
 print(cars)
 
+print('----------')
+
+# Write a factors function that accepts a positive whole number
+# It should return a list of all of the number's factors in ascending order
+# HINT: Could the range function be helpful here? Or maybe a while loop?
+#
+# factors(1)  => [1]
+# factors(2)  => [1, 2]
+# factors(10) => [1, 2, 5, 10]
+# factors(64) => [1, 2, 4, 8, 16, 32, 64]
+
+def factors(number):
+    start_number = 1
+    my_list = []
+    for num in range(start_number, number + 1):
+        if number % num == 0:
+            my_list.append(num)
+    return my_list
+
+print(factors(1))
+print(factors(2))
+print(factors(10))
+print(factors(64))
