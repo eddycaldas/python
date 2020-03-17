edbc43d2fac43064c5d1e134802c34593637394f
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-17 09:52:14 -0600

    pop method

---
+++ 11-Lists-Mutation/the-pop-method.py
@@ -0,0 +1,10 @@
+# pop method remove element from a list. if no argument provided, it removes the last one on the list.
+
+store_brands = ['king soopers', 'walgreens', 'safeway', 'albertsons']
+store_brands.pop()
+print(store_brands)
+print()
+
+my_brand = store_brands.pop(1)
+print(store_brands)
+print(my_brand)
