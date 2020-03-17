9ac882ff480c466c448b647a4e0e4fc19381e01f
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-17 10:17:31 -0600

    delete mothod

---
+++ 11-Lists-Mutation/the-del-keyword.py
@@ -0,0 +1,9 @@
+# delete keyword will remove elements based on their index position. Use : 
+
+
+soup = ['miso', 'french onion', 'clam chowder', 'chicken noodle', 'wonton', 'sopa de pollo']
+del soup[1] # ----> this is the syntax
+print(soup)
+
+del soup[1:3]
+print(soup)
