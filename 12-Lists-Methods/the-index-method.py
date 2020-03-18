2527e5a684c8938991b8bd66b11af672a7ca111f
Eddy Caldas <eddycaldas@Eddys-MacBook-Pro.local>
2020-03-18 12:06:04 -0600

    index method

---
+++ 12-Lists-Methods/the-index-method.py
@@ -0,0 +1,18 @@
+# it returns the first index position of the element in question. the first argument is the element looking for, the second one is the index at where we can to start the search.
+
+pizza_toppings = [
+    'pineapple',
+    'pepperoni',
+    'sausage',
+    'olive',
+    'sausage',
+    'olive'
+]
+
+print(pizza_toppings.index('pepperoni'))
+print(pizza_toppings.index('olive'))
+print()
+print(pizza_toppings.index('olive', 3))
+
+
+
